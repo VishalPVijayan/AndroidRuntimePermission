2898f5f5fd3b3c8a584cd387ae8375d880ec52c4
Vishal P Vijayan <vishalpvijayan711@gmail.com>
2020-07-20 15:52:37 +0530

    Second Stage Development

--- app/src/main/java/com/vishalpvijayan/androidruntimepermission/MainActivity.java
+++ app/src/main/java/com/vishalpvijayan/androidruntimepermission/MainActivity.java
@@ -3,12 +3,41 @@ package com.vishalpvijayan.androidruntimepermission;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSinglePermission, btnMultiplePermission;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Define Buttons
+        DefineWidgets();
     }
+
+    private void DefineWidgets() {
+        btnSinglePermission = findViewById(R.id.btnSinglePermission);
+        btnMultiplePermission = findViewById(R.id.btnMultiplePermission);
+
+        btnSinglePermission.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Single permission
+
+            }
+        });
+
+        btnMultiplePermission.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Multiple permissions
+
+            }
+        });
+    }
+
 }
