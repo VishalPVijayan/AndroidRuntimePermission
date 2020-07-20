6828a92e9e9f2c0f53ed9a89df74fd83d5c332b7
Vishal P Vijayan <vishalpvijayan711@gmail.com>
2020-07-20 16:39:29 +0530

    Fourth Stage Development

--- app/src/main/java/com/vishalpvijayan/androidruntimepermission/MainActivity.java
+++ app/src/main/java/com/vishalpvijayan/androidruntimepermission/MainActivity.java
@@ -10,11 +10,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
 import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -57,6 +61,18 @@ public class MainActivity extends AppCompatActivity {
                 });
                 break;
             case R.id.btnMultiplePermission:
+                Dexter.withContext(MainActivity.this).withPermissions(Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE).withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+
+                    }
+                });
                 break;
         }
     }
