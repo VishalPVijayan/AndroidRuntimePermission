f9337e156efc6394234ef07eaf29ad68aa64c9c0
Vishal P Vijayan <vishalpvijayan711@gmail.com>
2020-07-20 16:19:15 +0530

    third Stage Development

--- app/src/main/java/com/vishalpvijayan/androidruntimepermission/MainActivity.java
+++ app/src/main/java/com/vishalpvijayan/androidruntimepermission/MainActivity.java
@@ -1,43 +1,63 @@
 package com.vishalpvijayan.androidruntimepermission;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnSinglePermission, btnMultiplePermission;
+
+    @BindView(R.id.btnSinglePermission)
+    Button btnSinglePermission;
+    @BindView(R.id.btnMultiplePermission)
+    Button btnMultiplePermission;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Define Buttons
-        DefineWidgets();
+        ButterKnife.bind(this);
     }
 
-    private void DefineWidgets() {
-        btnSinglePermission = findViewById(R.id.btnSinglePermission);
-        btnMultiplePermission = findViewById(R.id.btnMultiplePermission);
+    @OnClick({R.id.btnSinglePermission, R.id.btnMultiplePermission})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btnSinglePermission:
+                Dexter.withContext(MainActivity.this).withPermission(Manifest.permission.CAMERA).withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        Toast.makeText(MainActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                    }
 
-        btnSinglePermission.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Single permission
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                        Toast.makeText(MainActivity.this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                    }
 
-            }
-        });
-
-        btnMultiplePermission.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Multiple permissions
-
-            }
-        });
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                });
+                break;
+            case R.id.btnMultiplePermission:
+                break;
+        }
     }
-
 }
